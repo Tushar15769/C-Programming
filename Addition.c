79aae7e7f3c896eda0099d2e3d99e69bc7a10c20
Tushar15769 <tushar.18246@gmail.com>
2025-10-19 12:18:44 +0530

    Addition

--- Addition.c
+++ Addition.c
@@ -1,5 +1,15 @@
 #include<stdio.h>
 int main()
 {
-   return 0; 
+    int a , b, c;
+printf("Enter your first number=");
+scanf("%d", &a);
+
+printf("Enter your second number=");
+scanf("%d", &b);
+
+c=a+b;
+printf("a+b= %d" , c);
+
+       
 }
