01480ee04292b8376beca97ea39cf34268b24428
Tushar15769 <tushar.18246@gmail.com>
2025-10-21 19:34:15 +0530

    Printing_pattern6

---
+++ Printing_pattern6.c
@@ -0,0 +1,29 @@
+
+
+    //            1
+    //          2 2 2
+    //        3 3 3 3 3
+    //      4 4 4 4 4 4 4    
+    //    5 5 5 5 5 5 5 5 5
+    //  6 6 6 6 6 6 6 6 6 6 6
+
+
+#include<stdio.h>
+int main()
+{
+     int i, j, k, n,c=80;
+     printf("Enter the value of n=");
+     scanf("%d",&n);
+        for(i=1; i<=n; i++)
+    {
+        for(j=1; j<=c/2-i; j++)
+        {
+            printf("  ");
+        }
+        for(k=1; k<=2*i-1; k++)
+        {
+            printf(" %d",i);
+        }
+        printf("\n");
+    }
+}
