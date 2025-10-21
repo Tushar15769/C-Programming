9544304154f7459ddc896f3a47feb5c971330ba9
Tushar15769 <tushar.18246@gmail.com>
2025-10-21 23:53:42 +0530

    Pattern

--- Hello.c
+++ Hello.c
@@ -12,30 +12,10 @@ int main()
         }
         for(k=1;k<=2*i-1;k++)
         {
-        printf("*");
+        printf(" *");
         }
         printf("\n");
     }  
 }
 
 
-
-#include<stdio.h>
-int main()
-{
-     int i, j, k, n,c=80;
-     printf("Enter the value of n=");
-     scanf("%d",&n);
-        for(i=1; i<=n; i++)
-    {
-        for(j=1; j<=c/2-i; j++)
-        {
-            printf("  ");
-        }
-        for(k=1; k<=2*i-1; k++)
-        {
-            printf(" %d",i);
-        }
-        printf("\n");
-    }
-}
