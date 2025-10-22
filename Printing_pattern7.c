e3d4b0d7169dff1eeacd67e7d55a1070d8d8e3f6
Tushar15769 <tushar.18246@gmail.com>
2025-10-22 15:29:30 +0530

    Patterns

---
+++ Printing_pattern7.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+    int i, j, k;
+    for(i=1;i<=5;i++)
+    {
+        for(j=1;j<=i-1;j++)
+        {
+        printf("  ");
+        }
+        for(k=i;k<=5;k++)
+        {
+        printf(" *");
+        }
+        printf("\n");
+    }  
+}
+
+
+
