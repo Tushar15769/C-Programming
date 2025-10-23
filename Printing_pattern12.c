49ec1579ab74a40eb2e51af358f8234c614d4d6b
Tushar15769 <tushar.18246@gmail.com>
2025-10-23 17:16:08 +0530

    Palindrome

--- Printing_pattern12.c
+++ Printing_pattern12.c
@@ -1,19 +1,19 @@
+
+// 0  1  1  2  3  5  8 
+
+
 #include<stdio.h>
 int main()
 {
-     int  i, j,k,sum,c=80;
-     for(i;i;i++)
+     int i,a=0,b=1,c,n;
+     printf("Enter the limit=");
+     scanf("%d", &n);
+     for(i=1;i<=n;i++)
      {
-       for(j;j;j++)
-       {
-
-       }
-       for(k;k;k++)
-       {
-
-
-       }
+       printf(" %d ", a);
+       c=a+b;
+       a=b;
+       b=c;
      }
-     printf("");
-     
+    
 }
