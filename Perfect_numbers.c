a9b6eaee84441723d5fd69255b7cb87c219a3d08
Tushar15769 <tushar.18246@gmail.com>
2025-10-23 14:45:13 +0530

    Perfect number

---
+++ Perfect_numbers.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+   void main()
+   {
+       int i,n,sum=0;
+       printf("Enter the value of n=");
+       scanf("%d",&n);
+       for(i=1;i<n;i++)
+       {
+        if(n%i==0)
+        {
+        sum=sum+i;
+        }
+        }
+         if(n==sum)
+           printf("Perfect number\n");
+        else
+          printf("Not a perfect number");
+    
+    }
+         
+
+
+
+
+
+
+
---
+++ Printing_pattern12.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+     int  i, j,k,sum,c=80;
+     for(i;i;i++)
+     {
+       for(j;j;j++)
+       {
+
+       }
+       for(k;k;k++)
+       {
+
+
+       }
+     }
+     printf("");
+     
+}
