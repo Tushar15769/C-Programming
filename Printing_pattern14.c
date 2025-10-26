8bcc5910bf4e9b7e5a2d5aea06d5c73b2f58a607
Tushar15769 <tushar.18246@gmail.com>
2025-10-26 17:04:32 +0530

    Patterns

---
+++ Printing_pattern14.c
@@ -0,0 +1,34 @@
+//  *  *  *  *  * 
+//  *  *  *  * 
+//  *  *  * 
+//  *  * 
+//  * 
+//  *  * 
+//  *  *  * 
+//  *  *  *  * 
+//  *  *  *  *  *  
+ 
+
+#include<stdio.h>
+   void main()
+   {
+     int i,j,k,l,m;
+      for(i=1;i<=5;i++)
+        {
+        for(l=i;l<=5;l++)
+          {
+           printf(" * ");
+          }
+           printf("\n");
+        }
+         for(k=2;k<=5;k++)
+        {
+             for(m=1;m<=k;m++)
+            {
+            printf(" * ");
+            }
+            printf("\n");
+        }
+    }
+
+   
