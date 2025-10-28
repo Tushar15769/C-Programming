1341e7184a28dac2089f8a24e17c4ead730e3c29
Tushar15769 <tushar.18246@gmail.com>
2025-10-28 19:53:10 +0530

    Patterns

--- Printing_pattern17.c
+++ Printing_pattern17.c
@@ -1,43 +1,30 @@
+
+//          *
+//        *   *
+//      *       *
+//    *           *
+//  * * * * * * * * *
+ 
+
 #include<stdio.h>
    void main()
    {
-     int i,j,k,l,m,n=5,o,p,q,r;
+     int i,j,k,n=5;
       for(i=1;i<=n;i++)
         {
-        for(j=1;j<=i;j++)
-          {
-           printf(" * ");
+           for(j=1;j<=n-i;j++)
+           {
+           printf("  ");
+           }
+           for(j=1;j<=2*i-1;j++)
+           {
+           if(i==n || j==2*i-1 || j==1)
+           {
+            printf(" *");
+           }
+           else
+           printf("  ");
           }
            printf("\n");
-        }
-         for(k=4;k>=1;k--)
-        {
-             for(l=1;l<=k;l++)
-            {
-            printf(" * ");
-            }
-            printf("\n");
-        }
-        for(m=1;m<=n;m++)
-        {
-            for(o=1;o<=k;o++)
-            {
-            printf("  ");
-            }
-             for(p=1;p<=n;p++)
-            {
-            printf(" * ");
-            }
-             printf("\n");
-        }
-             for()
-             {
-
-
-
-
-             }
-
-
         }
     }
