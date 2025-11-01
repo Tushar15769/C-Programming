ec1eec04df13ddc4d9ee7ebd4d2d6db5ddb6690d
Tushar15769 <tushar.18246@gmail.com>
2025-11-01 12:24:42 +0530

    Patterns

---
+++ Printing_pattern22.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+   void main()
+   {
+     int i,j,k,l,n=5;
+      for(i=1;i<=n;i++)
+        {
+           for(j=1;j<=n-i;j++)
+           {
+           printf("  ");
+           }
+           for(j=1;j<=2*i-1;j++)
+           {
+           if(i==n || j==2*i-1 || j==1)
+           {
+            printf(" *");
+           }
+           else
+           printf("  ");
+           }
+           printf("\n");
+        }
+           for(k=1;k<=n;k++)
+           { 
+             for(l=1;l<=k-1;l++)
+             {
+             printf("  ");
+             }
+             for(l=1;l<=2*(n-k)+1;l++)
+             {
+             if(k==1 || l==2*(n-k)+1 || l==1)
+             {
+              printf(" *");
+             }
+             else
+             printf("  ");
+            }
+             printf("\n");
+            }
+    }
