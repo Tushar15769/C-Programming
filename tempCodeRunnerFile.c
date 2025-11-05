afb467faf978cbca08e46a9ac7d2cfd3526250a3
Tushar15769 <tushar.18246@gmail.com>
2025-11-05 10:44:46 +0530

    Patterns

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,12 +1,18 @@
-
-        //  for(l=2;l<=n;l++)
-        // {
-        //      for(m=2;m<n-l;m++)
-        //     {
-        //     printf("  ");
-        //     }
-        //     for(o=1;o<=m;o++)
-        //     {
-        //         printf(" * ");
-        //     }
-        //     printf("\n");
+   
+   #include<stdio.h>
+       void main()
+     {   
+         int l,m,n,o;
+          for(l=2;l<=n;l++)
+         {
+             for(m=2;m<n-l;m++)
+            {
+            printf("  ");
+            }
+            for(o=1;o<=m;o++)
+             {
+                 printf(" * ");
+             }
+            printf("\n");
+         }
+     }
