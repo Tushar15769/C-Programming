23c2a0817b7428d867402f57b180cc5b44c3c5e7
Tushar15769 <tushar.18246@gmail.com>
2025-11-07 20:25:37 +0530

    Insertion of element

---
+++ Array.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+    void main()
+    {
+       int arr[50],n,i,key,loc;
+       printf("Enter the size:");
+       scanf("%d",&n);
+       printf("Enter the %d elements:",n);
+       for(i=0;i<n;i++)
+       {
+        scanf("%d",&arr[i]);
+       }
+        printf("Enter element to insert:");
+        scanf("%d",&key);
+        printf("Enter the location to insert:");
+        scanf("%d",&loc);
+        for(i=n-1;i>=loc;i--)
+        {
+         arr[i+1]=arr[i];
+        }
+        arr[loc]=key;
+        printf("Array after insertion:\n");
+        for(i=0;i<=n;i++)
+        {
+          printf(" %d",arr[i]);  
+        }
+    }
