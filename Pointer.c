c24f6667f99e99c7ed12b665ebdc5dbe9d36c59a
Tushar15769 <tushar.18246@gmail.com>
2025-11-24 20:00:16 +0530

    Pointers basic

---
+++ Pointer.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int i=100;
+int*ptr;
+ptr=&i;
+printf("%d\n",i);
+printf("%u\n",ptr);
+printf("%u\n",&i);
+printf("%u\n",&ptr);
+printf("%d\n",*ptr);
+printf("%d\n",*(&i));
+}
