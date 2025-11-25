2d4b9ef93c9ac3b6aed1dc140e776ec53c1dc48c
Tushar15769 <tushar.18246@gmail.com>
2025-11-25 20:43:11 +0530

    Size of pointers

---
+++ Pointer2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+struct Emp{
+    int eno;
+    char ename;
+    float esal;
+};
+void main()
+{
+char *cp;
+int *ip;
+struct Emp *sep;
+printf("size of the char*:%d\n",sizeof(cp));
+printf("size of the ip*:%d\n",sizeof(ip));
+printf("size of the sep*:%d\n",sizeof(sep));
+}
