cd85c22361670d7e8dacc2d1983d495e84515010
Tushar15769 <tushar.18246@gmail.com>
2025-11-29 16:06:08 +0530

    malloc

--- Pointer5.c
+++ Pointer5.c
@@ -2,14 +2,14 @@
 #include<stdlib.h>
 struct Emp{
   int eno;
-  char ename;
+  char ename[20];
   float esal;
 };
-void main()
+int main()
 {
 struct Emp* ptr;
 ptr=(struct Emp*)malloc(sizeof(struct Emp));
-if(ptr=NULL)
+if(ptr==NULL)
 {
     printf("Out of memory error\n");
 }
@@ -17,8 +17,10 @@ else
 {
     printf("Enter Emp details:");
     scanf("%d\t%s\t%f",&ptr->eno,ptr->ename,&ptr->esal);
-     
+
+    printf("Employee Details:\n"); 
     printf("%d\t%s\t%f",ptr->eno,ptr->ename,ptr->esal);  
 }
  free(ptr);
+ return 0;
 }
