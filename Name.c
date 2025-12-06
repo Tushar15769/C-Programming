87e6e60d3686c34324f3310db607d7e8b5295903
Tushar15769 <tushar.18246@gmail.com>
2025-12-06 18:09:44 +0530

    Name

---
+++ Name.c
@@ -0,0 +1,134 @@
+//  * * * * * 
+//      *     
+//      *     
+//      *     
+//      *     
+
+//  *       * 
+//  *       * 
+//  *       * 
+//  *       * 
+//  * * * * * 
+
+//  * * * * * 
+//  *
+//  * * * * * 
+//          * 
+//  * * * * * 
+
+//  *       * 
+//  *       * 
+//  * * * * * 
+//  *       * 
+//  *       * 
+
+//  * * * * * 
+//  *       * 
+//  * * * * * 
+//  *       * 
+//  *       * 
+
+//  * * * * * 
+//  *       * 
+//  * * * * * 
+//  *     *   
+//  *       * 
+
+
+
+#include<stdio.h>
+void main()
+{
+  int i,j;
+  int n=5;
+  for(i=1;i<=n;i++)
+  {
+     for(j=1;j<=n;j++)
+    {
+      if(i==1 || j==3 )
+      {
+       printf("* ");
+      }
+      else{
+        printf("  ");
+      }
+    }
+    printf("\n");
+  } 
+  printf("\n");
+  for(i=1;i<=n;i++)
+  {
+     for(j=1;j<=n;j++)
+    {
+      if(j==1 || j==n || i==n)
+      {
+       printf("* ");
+      }
+      else{
+        printf("  ");
+      }
+    }
+    printf("\n");
+  }
+  printf("\n");
+  for(i=1;i<=n;i++)
+  {
+     for(j=1;j<=n;j++)
+    {
+      if(i==1 || i==3 || i==n || j==1 && i==2 || j==n && i==4  )
+      {
+       printf("* ");
+      }
+      else{
+        printf("  ");
+      }
+    }
+    printf("\n");
+  }
+  printf("\n");
+  for(i=1;i<=n;i++)
+  {
+     for(j=1;j<=n;j++)
+    {
+      if(j==1 || j==n || i==3 )
+      {
+       printf("* ");
+      }
+      else{
+        printf("  ");
+      }
+    }
+    printf("\n");
+  }
+  printf("\n");
+   for(i=1;i<=n;i++)
+  {
+     for(j=1;j<=n;j++)
+    {
+      if(i==1 || j==1 || j==n || i==3 )
+      {
+       printf("* ");
+      }
+      else{
+        printf("  ");
+      }
+    }
+    printf("\n");
+  }
+  printf("\n");
+   for(i=1;i<=n;i++)
+  {
+     for(j=1;j<=n;j++)
+    {
+      if(i==1 || j==1 || i==3 || j==n && i==2 || j==n-1 && i==4 || j==n && i==5)
+      {
+       printf("* ");
+      }
+      else{
+        printf("  ");
+      }
+    }
+    printf("\n");
+  }
+  printf("\n");
+}
