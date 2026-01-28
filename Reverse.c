84d9592502b7dcac28ce18fa0595c0ef6bd1a27d
Tushar15769 <tushar.18246@gmail.com>
2026-01-28 12:53:07 +0530

    Reversing of an array

---
+++ Reverse.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+  int n, i;
+  int arr[100];
+
+  printf("Enter the number of elements:");
+  scanf("%d", &n);
+
+  printf("Enter the %d elements:\n", n);
+  for (i = 0; i < n; i++)
+  {
+    scanf("%d", &arr[i]);
+  }
+  printf("Arrays elements in reverse order :\n");
+  for (i = n - 1; i >= 0; i--)
+  {
+    printf("\t%d", arr[i]);
+  }
+  return 0;
+}
