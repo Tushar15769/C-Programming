62cf2c32c872d79343e0ad5ab3a29f5d89844bda
Your Name <you@example.com>
2026-02-14 19:01:29 +0530

    How to create synonym to user defined datatype

--- typedef2.c
+++ typedef2.c
@@ -1,27 +1,27 @@
-#include<stdio.h>
-#include<stdlib.h>
-typedef char* string;
+#include <stdio.h>
+#include <stdlib.h>
+typedef char *string;
 string read(void);
 int main(void)
 {
-  
-    string name;
-    name=read();
-    if(name!=NULL)
-    {
-      printf("Welcome %s\n",name);
-      free(name);
-    }
-    return 0;
+
+  string name;
+  name = read();
+  if (name != NULL)
+  {
+    printf("Welcome %s\n", name);
+    free(name);
+  }
+  return 0;
 }
-  string read(void)
-   {
-       string name=malloc(50*sizeof(char));
-       if(name==NULL)
-       {
-        return NULL;
-       }
-       printf("Enter your name:");
-       fgets(name,50,stdin);
-       return name;
-    }
+string read(void)
+{
+  string name = malloc(50 * sizeof(char));
+  if (name == NULL)
+  {
+    return NULL;
+  }
+  printf("Enter your name:");
+  fgets(name, 50, stdin);
+  return name;
+}
